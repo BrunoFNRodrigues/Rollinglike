998d4459ba4dc6e862d518501f80204631a6e78e
Bruno Rodrigues <bf17669@gmail.com>
2022-10-05 02:12:56 -0300

    Troca de cena

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactionAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange)
+        {
+            if(Input.GetKeyDown(interactKey))
+            {
+                interactionAction.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+    }
+}
